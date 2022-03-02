797448d98ae6ca31793fc96e38508121947ed05f
Rafer <rafer.gluyas@gmail.com>
2022-03-02 14:33:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,4 +16,5 @@ exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE:-added}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
-      -level="${INPUT_LEVEL}"
+      -level="${INPUT_LEVEL}" \
+ | grep 'error: '
