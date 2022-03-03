23dca7ee0657d5292b83cb539747aebf618b1f0a
David Adams <dbadams@iinet.net.au>
2022-03-03 12:22:38 +0800

    a fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,10 @@ echo $INPUT_FILE_LIST
 for input_file in ${INPUT_FILE_LIST}; do
   echo $input_file
 done
+declare -a strarr=($INPUT_FILE_LIST)
 
 total_violations=0
-for input_file in ${INPUT_FILE_LIST}; do
+for input_file in ${strarr}; do
   if [[ "$input_file" == *.java ]]; then
     found_v=$(exec java -jar /checkstyle.jar "${input_file}" -c "${INPUT_CHECKSTYLE_CONFIG}" ${OPT_PROPERTIES_FILE} -f xml \
      | reviewdog -f=checkstyle \
